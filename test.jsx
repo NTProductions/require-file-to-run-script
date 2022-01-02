cae055964d80d6d21b53d04170c8188d331df37d
Nate Lovell <Natelovell99@gmail.com>
2022-01-01 22:10:30 -0700

    final files

---
+++ test.jsx
@@ -0,0 +1,21 @@
+﻿var window = new Window("palette", "My Script", undefined);
+window.orientation = "column";
+
+var text = window.add("statictext", undefined, "Some sample text");
+var button = window.add("button", undefined, "Start");
+
+main();
+
+function main() {
+    var filePath = $.fileName;
+    filePath = filePath.slice(0, filePath.lastIndexOf("/"))+"/image.png";
+    
+    if(!File(filePath).exists) {
+        alert("No 'Image.png' file detected!");
+        return false;
+        } else {
+            window.center();
+            window.show();
+            }
+    
+    }
